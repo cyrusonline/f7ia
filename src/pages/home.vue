ce6a4ed396fc6f9b6d0d1f888dffdc0333a4ae64
Cyrus Chan <cyruschan@hsmc.edu.hk>
2018-08-09 09:28:54 +0800

    some update

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,7 +3,11 @@
     <f7-navbar sliding title="Home"></f7-navbar>
     <f7-block-title>{{ title }}</f7-block-title>
     <f7-block inner>
-      <f7-button raised fill @click="buy" >QR</f7-button>
+      <f7-button raised fill @click="buy(sku_add_mult)" >QR</f7-button>
+      <f7-list>
+      <f7-list-button v-for="product in products" :title="product.name"  @click="buy(product.id)"></f7-list-button>
+     
+    </f7-list>
     </f7-block>
   </f7-page>
 </template>
@@ -15,38 +19,54 @@ export default {
     return {
      
       title: 'Hello World',
+      products:[
+        
+        {name: 'add multiple',
+        id: 'sku_add_mult'},
+
+        {name: 'as gs',
+        id: 'sku_as_gt'},
+
+        {name: 'as basic',
+        id: 'sku_as_basic'},
+          
+      ],
+      products_created:['sku_add_mult','sku_as_gt','sku_as_basic'],
       product1: 'sku_add_mult',
       product3: 'sku_as_gt',
       product2: 'sku_as_basic ',
     };
   },
   methods:{
-    buy(){
+    buy(product){
       console.log('buy')
       inAppPurchase
-  .buy('sku_add_mult')
-  .then(function (data) {
-    console.log('inside buy')
-    console.log(data);
-    alert(JSON.stringify(products));
-    /*
-      {
-        transactionId: ...
-        receipt: ...
-        signature: ...
-      }
-    */
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+      .buy(product)
+      .then(function (data) {
+      console.log('inside buy')
+      console.log(data);
+      alert(JSON.stringify(products));
+        /*
+          {
+            transactionId: ...
+            receipt: ...
+            signature: ...
+          }
+        */
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    },
+    enableItems(id){
+
     }
 
   },
   created(){
 
 inAppPurchase
-  .getProducts(['sku_add_mult', 'sku_as_gt', ])
+  .getProducts(this.products_created)
   .then(function (products) {
     alert(JSON.stringify(products));
     console.log(products)
